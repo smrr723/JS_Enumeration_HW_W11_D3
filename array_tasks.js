7917875db691776da1b9e8dbd1ebd73b45161931
Scott Murray <rai.five@gmail.com>
2017-11-29 21:04:58 +0000

    Woop Woop

--- array_tasks.js
+++ array_tasks.js
@@ -1,40 +1,75 @@
 var arrayTasks = {
 
-	concat: function (arr1, arr2) {
+	concat: (arr1, arr2) => {
 		return arr1.concat(arr2);
 	},
 
-	insertAt: function (arr, itemToAdd, index) {
+
+	insertAt: (arr, itemToAdd, index) => {
 		arr.splice(index, 0, itemToAdd);
 		return arr;
 	},
 
-	square: function (arr) {
+
+	square: (arr) => {
 		var squared = arr.map(arrItem => arrItem * arrItem);
 		return squared;
 	},
+
 
 	sum: function (arr) {
 		var summed = arr.reduce((accumulator, currentValue) => accumulator + currentValue);
 		return summed;
 	},
 
-	findDuplicates: function (arr) {
 
+	findDuplicates: (arr) => {
+		var tempArr = []
+		for (item of arr){
+			if(arr.includes(item, item) && !tempArr.includes(item)){
+				tempArr.push(item);
+			}
+		}
+		return tempArr;
 	},
 
-	// removeAndClone: function (arr, valueToRemove) {
 
-	// },
+	removeAndClone: (arr, valueToRemove) => {
+		var tempArr = []
+		for (item of arr){
+			if(item !== valueToRemove){
+				tempArr.push(item);
+			}
+		}
+		return tempArr;
+	},
 
-	// findIndexesOf: function (arr, itemToFind) {
 
-	// },
+	findIndexesOf: (arr, itemToFind) => {
+		var tempArr = [];
+		arr.forEach((item, index, arr) => {
+			if(item == itemToFind){
+				tempArr.push(index);
+			}
+		})
+		return tempArr;
+	},
 
-	// sumOfAllEvenNumbersSquared: function (arr) {
+// tried below first, but was returning [0,0,0], i.e. just getting stuck on the first instance of 1..??
+		// var tempArr = []
+		// for (item of arr){
+		// 	if(item === itemToFind){
+		// 		tempArr.push(arr.indexOf(item));
+		// 	}
+		// }
+		// return tempArr;
 
-	// }
-
+	sumOfAllEvenNumbersSquared: (arr) => {
+		var filtered = arr.filter(arrItem => arrItem % 2 === 0);
+		return arrayTasks.sum(arrayTasks.square(filtered));
+	}
 }
+
+
 
 module.exports = arrayTasks
